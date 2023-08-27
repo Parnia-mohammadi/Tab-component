3d5f603f5de82be545b2f55e7dde937dd6853ff5
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-08-27 20:27:11 +0430

    using state in interactive tab

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,27 @@ const tabData = [
 ];
 
 function App() {
-return <h1>Tab Component : starter file</h1>
+  const [activeTab, setActiveTab] = useState(1);
+  const handleActiveTab = (id) => {
+    setActiveTab(id);
+  };
+  return (
+    <div className="tab">
+      <h1>Tab Component : starter file</h1>
+      <div className="tab__header">
+        {tabData.map((data) => (
+          <button
+            key={data.id} className={activeTab == data.id ? "active" : ""}
+            onClick={() => handleActiveTab(data.id)}
+          >
+            <span>{data.title}</span>
+            <span className="tab-indicator"></span>
+          </button>
+        ))}
+      </div>
+      <div className="tab__content">{tabData[activeTab - 1].content}</div>
+    </div>
+  );
 }
 
 export default App;

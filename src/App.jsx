8b1d44294d6781c33772581b6f552586b8c55773
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-08-28 18:09:52 +0430

    adding another state to change first state

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,35 @@ const tabData = [
 
 function App() {
   const [activeTab, setActiveTab] = useState(1);
+  const [isTabOn, setIsTabOn] = useState(true);
   const handleActiveTab = (id) => {
     setActiveTab(id);
   };
   return (
-    <div className="tab">
-      <h1>Tab Component : starter file</h1>
-      <div className="tab__header">
-        {tabData.map((data) => (
-          <button
-            key={data.id} className={activeTab == data.id ? "active" : ""}
-            onClick={() => handleActiveTab(data.id)}
-          >
-            <span>{data.title}</span>
-            <span className="tab-indicator"></span>
-          </button>
-        ))}
-      </div>
-      <div className="tab__content">{tabData[activeTab - 1].content}</div>
+    <div>
+      {isTabOn ? (
+        <div className="tab">
+          <h1>Tab Component : starter file</h1>
+          <div className="tab__header">
+            {tabData.map((data) => (
+              <button
+                key={data.id}
+                className={activeTab == data.id ? "active" : ""}
+                onClick={() => handleActiveTab(data.id)}
+              >
+                <span>{data.title}</span>
+                <span className="tab-indicator"></span>
+              </button>
+            ))}
+          </div>
+          <div className="tab__content">{tabData[activeTab - 1].content}</div>
+        </div>
+      ) : (
+        <div>
+          <p>tab is off now</p>
+        </div>
+      )}
+      <button onClick={() => setIsTabOn(!isTabOn)}>TabOFF</button>
     </div>
   );
 }
